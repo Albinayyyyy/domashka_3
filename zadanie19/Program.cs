0b64f2ecd247f6f82002cee65bda42b022c697a4
Albina <albinasannikova71@gmail.com>
2022-10-14 00:48:01 +0300

    final

---
+++ zadanie19/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length(Console.ReadLine()!);
+if (len == 5)
+{
+    while (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
